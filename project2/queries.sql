c26884e668df9de7793396f5ca821a87b1b102f1
Calvin Chan <calvin.chan.h@gmail.com>
2015-01-31 23:51:16 -0800

    All queries work except 4

--- project2/queries.sql
+++ project2/queries.sql
@@ -2,9 +2,8 @@ SELECT COUNT(*)
 	FROM Users;
 SELECT COUNT(*) 
 	FROM Items INNER JOIN Location ON location_id = Location.id 
-	WHERE Location.location = 'New York';
-SELECT COUNT(*) 
-	FROM (SELECT COUNT(*) as count FROM Items_Categories GROUP BY item_id) AS auctions
+	WHERE BINARY Location.location = 'New York';
+SELECT COUNT(*) FROM (SELECT COUNT(*) as count FROM Items_Categories GROUP BY item_id) AS auctions
 	WHERE count = 4;
 SELECT id FROM (
 	SELECT id as id, MAX(currently) as max 
@@ -16,8 +15,8 @@ SELECT COUNT(*) FROM Users WHERE seller_rating > 1000;
 SELECT COUNT(*) FROM Users WHERE seller_rating IS NOT NULL AND bidder_rating IS NOT NULL;
 
 SELECT Items.id, MAX(amount) as highest_bid FROM Items INNER JOIN Bids ON Bids.item_id = Items.id GROUP BY Items.id
-SELECT COUNT(*) 
+SELECT COUNT(*) from (SELECT * 
 	FROM Items_Categories INNER JOIN (
 		SELECT * FROM (
 			SELECT Items.id, MAX(amount) as highest_bid FROM Items INNER JOIN Bids ON Bids.item_id = Items.id GROUP BY Items.id) AS Items_Bids WHERE Items_Bids.highest_bid > 100) 
-	as Max_Items_Bids ON Items_Categories.item_id = Max_Items_Bids.id GROUP BY category_id;
+	as Max_Items_Bids ON Items_Categories.item_id = Max_Items_Bids.id GROUP BY category_id) as Items_Cats;
