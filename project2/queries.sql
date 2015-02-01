4d72e6476a867617f3e52f0663b6afe24901d971
Calvin Chan <calvin.chan.h@gmail.com>
2015-02-01 01:20:50 -0800

    Optimized queries 4 and 7

--- project2/queries.sql
+++ project2/queries.sql
@@ -7,17 +7,13 @@ SELECT COUNT(*) FROM (SELECT COUNT(*) as count FROM Items_Categories GROUP BY it
     WHERE count = 4;
 SELECT id AS ITEMID FROM (
     SELECT id, amount FROM Items INNER JOIN Bids ON Items.id = Bids.item_id WHERE ends > '2001-12-20 00:00:01' GROUP BY Items.id
-) as Items_Bids WHERE amount = (
-    SELECT MAX(amount) FROM (
-        SELECT id, amount FROM Items INNER JOIN Bids ON Items.id = Bids.item_id WHERE ends > '2001-12-20 00:00:01' GROUP BY Items.id
-    ) as Max_Bid
-);
+) as Max_Items_Bids 
+    ORDER BY amount DESC
+    LIMIT 1;
 SELECT COUNT(*) FROM Users WHERE seller_rating > 1000;
 SELECT COUNT(*) FROM Users WHERE seller_rating IS NOT NULL AND bidder_rating IS NOT NULL;
-SELECT COUNT(DISTINCT category_id) AS 'COUNT(DISTINCT CATEGORY)' FROM (
-    SELECT category_id FROM Items_Categories INNER JOIN (
-        SELECT * FROM (
-            SELECT Items.id, MAX(amount) as highest_bid FROM Items INNER JOIN Bids ON Bids.item_id = Items.id GROUP BY Items.id
-        ) AS Items_Bids WHERE Items_Bids.highest_bid > 100
+SELECT COUNT(DISTINCT CATEGORY) FROM (
+    SELECT category_id AS CATEGORY FROM Items_Categories INNER JOIN (
+        SELECT Items.id, MAX(amount) as highest_bid FROM Items INNER JOIN Bids ON Bids.item_id = Items.id WHERE amount > 100 GROUP BY Items.id
     ) as Max_Items_Bids ON Items_Categories.item_id = Max_Items_Bids.id GROUP BY category_id
-) as Items_Cats;
+) as Categories_GT100;
