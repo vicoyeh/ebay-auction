4af8eae41daef5aa11e13104d3f8ecf90ffeea1c
Vic <vic317_yeh@hotmail.com>
2015-02-09 13:55:01 -0800

    build index

--- project3/project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
+++ project3/project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -3,6 +3,7 @@ package edu.ucla.cs.cs144;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.File;
+import java.util.ArrayList;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,6 +22,9 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+
+import edu.ucla.cs.cs144.Item;
+import edu.ucla.cs.cs144.DbManager;
 
 public class Indexer {
     
@@ -46,15 +50,12 @@ public class Indexer {
     }
 
     public void indexItem(Item item) throws IOException {
-
         System.out.println("Indexing item: " + item);
         IndexWriter writer = getIndexWriter(false);
         Document doc = new Document();
-        doc.add(new StringField("name", hotel.name, Field.Store.YES));
-        doc.add(new StringField("name", hotel.category, Field.Store.YES));
-        doc.add(new StringField("city", hotel.description, Field.Store.NO));
-        String fullSearchableText = hotel.name + " " + hotel.category) + " " + hotel.description;
-        doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+        doc.add(new StringField("name", item.name, Field.Store.YES));
+        doc.add(new StringField("category", item.category, Field.Store.YES));
+        doc.add(new TextField("description", item.description, Field.Store.NO));
         writer.addDocument(doc);
     }   
 
@@ -71,25 +72,30 @@ public class Indexer {
 	}
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
+	//retrieve data
+    //todo
+    PreparedStatement prepareRetrieveItems = con.PrepareStatement(
+        "SELECT Items.name, Items.description FROM Items INNER JOIN Categories"
+    );
 
+    String name, category, description;
+    ArrayList<Item> itemList = new ArrayList<Item>();
+    ResultSet rs = prepareRetrieveItems.executeQuery();
+    while (rs.next()) {
+        name = rs.getString("name");
+        category = rs.getString("category");
+        description = rs.getString("description");
+        Item item = new Item(name,category,description);
+        itemList.add(item);
+    }
+
+    //build index
+    for (Item item: itemList) {
+        indexItem(item);
+    }
+
+
+    closeIndexWriter();
 
         // close the database connection
 	try {
