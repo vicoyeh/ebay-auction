5db8c24547f3a094ed534bbec01eaae3f13199c4
Calvin <calvin.chan.h@gmail.com>
2015-02-13 21:04:53 -0800

    I think XML is done

--- project3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
+++ project3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -185,12 +185,154 @@ public class AuctionSearch implements IAuctionSearch {
 		}
 
 		try {
-			Statement retrieveQuery = conn.createStatement();
+	        PreparedStatement retrieveItem = conn.prepareStatement(
+	            "SELECT * FROM Items INNER JOIN Users ON seller_id = Users.id INNER JOIN Location ON location_id = Location.id WHERE Items.id = ?"
+	        );
+            retrieveItem.setString(1, itemId);
+            ResultSet rs = retrieveItem.executeQuery();
+            rs.first();
+            if (rs.getRow() != 0) {
+                org.w3c.dom.Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();                
 
-			//todo
+                Element root = doc.createElement("Item");
+                root.setAttribute("ItemID", itemId);
 
+                // name
+                Element itemName = doc.createElement("Name");
+                itemName.appendChild(doc.createTextNode(escapeSpecial(rs.getString("name"))));
+                root.appendChild(itemName);
 
+                // categories
+                PreparedStatement retrieveCategories = conn.prepareStatement(
+                    "SELECT category FROM Categories INNER JOIN Items_Categories ON id = category_id WHERE item_id = ?"
+                );
+                retrieveCategories.setString(1, itemId);
+                ResultSet categoriesRs = retrieveCategories.executeQuery();
+                while (categoriesRs.next()) {
+                    Element category = doc.createElement("Category");
+                    category.appendChild(doc.createTextNode(escapeSpecial(categoriesRs.getString("category"))));
+                    root.appendChild(category);
+                }
+                categoriesRs.close();
+                retrieveCategories.close();
 
+                // currently
+                Element currently = doc.createElement("Currently");
+                currently.appendChild(doc.createTextNode("$" + rs.getString("currently")));
+                root.appendChild(currently);
+    
+                // buyprice
+                String bp = rs.getString("buy_price");
+                if (bp != null) {
+                    Element buyPrice = doc.createElement("Buy_Price");
+                    buyPrice.appendChild(doc.createTextNode("$" + bp));
+                    root.appendChild(buyPrice);
+                }
+
+                // firstbid
+                Element firstBid = doc.createElement("First_Bid");
+                firstBid.appendChild(doc.createTextNode("$" + rs.getString("first_bid")));
+                root.appendChild(firstBid);
+
+                Element bids = doc.createElement("Bids");
+                PreparedStatement retrieveBids = conn.prepareStatement(
+                    "SELECT * FROM Bids INNER JOIN Users ON bidder_id = Users.id INNER JOIN Location ON Users.location_id = Location.id WHERE item_id = ?"
+                );
+                retrieve.setString(1, itemId);
+                ResultSet bidsRs = retrieveBids.executeQuery();
+                Integer numBids = 0;
+                while (bidsRs.next()) {
+                    Element bid = doc.createElement("Bid");
+
+                    // bidder
+                    Element bidder = doc.createElement("Bidder");
+                    bidder.setAttribute("UserID", escapeSpecial(bidsRs.getString("bidder_id")));
+                    bidder.setAttribute("Rating", bidresult.getString("bidder_rating"));
+                    String loc = bidsRs.getString("location");
+                    if (loc != null) {
+                        Element location = doc.createElement("Location");
+                        location.appendChild(doc.createTextNode(escapeSpecial(loc)));
+                        bidder.appendChild(location);
+                    }
+                    String country = bidsRs.getString("country");
+                    if (country != null) {
+                        Element country = doc.createElement("Country");
+                        country.appendChild(doc.createTextNode(escapeSpecial(country)));
+                        bidder.appendChild(country);
+                    }
+                    bid.appendChild(bidder);
+
+                    // time
+                    Element time = doc.createElement("Time");
+                    time.appendChild(doc.createTextNode(convertDate(bidresult.getString("time"), "yyyy-MM-dd HH:mm:ss", "MMM-dd-yy HH:mm:ss")));
+                    bid.appendChild(time);
+
+                    // amount
+                    Element amount = doc.createElement("Amount"); 
+                    amount.appendChild(doc.createTextNode(bidresult.getString("amount")));
+                    bid.appendChild(amount);
+
+                    bids.appendChild(bid);
+                    numBids++;
+                }
+
+                // numbids
+                Element numberBids = doc.createElement("Number_of_Bids");
+                numberBids.appendChild(doc.createTextNode(Integer.toString(numBids)));
+                root.appendChild(numberBids);
+                
+                // bids
+                root.appendChild(bids);
+                bidsRs.close();
+                retrieveBids.close();
+
+                // location
+                Element location = doc.createElement("Location");
+                location.appendChild(doc.createTextNode((escapeSpecial(rs.getString("location")))));
+                root.appendChild(location);
+
+                // country
+                Element country = doc.createElement("Country");
+                country.appendChild(doc.createTextNode((replacespecial(rs.getString("country")))));
+                root.appendChild(country);
+
+                // started
+                Element started = doc.createElement("Started");
+                started.appendChild(doc.createTextNode(convertDate(rs.getString("started"), "yyyy-MM-dd HH:mm:ss", "MMM-dd-yy HH:mm:ss")));
+                root.appendChild(started);
+
+                // ends
+                Element ends = doc.createElement("Ends");
+                ends.appendChild(doc.createTextNode(convertDate(result.getString("ends"), "yyyy-MM-dd HH:mm:ss", "MMM-dd-yy HH:mm:ss")));
+                root.appendChild(ends);
+
+                // seller
+                Element seller = doc.createElement("Seller");
+                seller.setAttribute("UserID", (escapeSpecial(rs.getString("seller_id"))));
+                seller.setAttribute("Rating", rs.getString("seller_rating"));
+                root.appendChild(seller);
+
+                // description
+                Element description = doc.createElement("Description");
+                description.appendChild(doc.createTextNode(replacespecial(rs.getString("description"))));
+                root.appendChild(description);
+
+                doc.appendChild(root);
+            }
+
+            // Write the XML
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StringWriter writer = new StringWriter();
+            StreamResult res = new StreamResult(writer);
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.transform(source, res);
+            xmlstore = writer.toString();
+
+            rs.close();
+            retrieveItem.close();
 
 		} catch (SQLException ex) {
 	        System.out.println(ex);
